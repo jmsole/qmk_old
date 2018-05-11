8212c2178746e7e455b37bad851490345c26282b
Jose Sole <capitan.leniz@gmail.com>
2018-05-11 01:27:44 +0100

    Finishid adding code to make Fn key work on Mac (from https://gist.github.com/fauxpark/98560909fe199ec93714289dd8313955)

--- tmk_core/protocol/usb_descriptor.h
+++ tmk_core/protocol/usb_descriptor.h
@@ -254,7 +254,11 @@ typedef struct
 # error "There are not enough available endpoints to support all functions. Remove some in the rules.mk file.(MOUSEKEY, EXTRAKEY, CONSOLE, NKRO, MIDI, SERIAL, STENO)"
 #endif
 
-#define KEYBOARD_EPSIZE             8
+#ifdef APPLE_FN_ENABLE
+  #define KEYBOARD_EPSIZE           9
+#else
+  #define KEYBOARD_EPSIZE           8
+#endif
 #define MOUSE_EPSIZE                8
 #define EXTRAKEY_EPSIZE             8
 #define RAW_EPSIZE                  32
