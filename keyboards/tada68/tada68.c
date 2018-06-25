948a6029dced13e57417163edc850abf6411db63
Jose Sole <capitan.leniz@gmail.com>
2018-06-25 22:35:36 +0100

    Changes after refactoring.

--- keyboards/tada68/tada68.c
+++ keyboards/tada68/tada68.c
@@ -15,17 +15,17 @@ void matrix_scan_kb(void) {
 };
 
 void led_init_ports(void) {
-    // * Set our LED pins as output
-    DDRB |= (1<<2);
+	// * Set our LED pins as output
+	DDRB |= (1<<2);
 }
 
 void led_set_kb(uint8_t usb_led) {
 	if (usb_led & (1<<USB_LED_CAPS_LOCK)) {
-        // Turn capslock on
-        PORTB &= ~(1<<2);
-    } else {
-        // Turn capslock off
-        PORTB |= (1<<2);
-    }
+		// Turn capslock on
+		PORTB &= ~(1<<2);
+	} else {
+		// Turn capslock off
+		PORTB |= (1<<2);
+	}
 	led_set_user(usb_led);
 }
