7f749d46c46e8507266cb5ff0d4a500d8ebed62a
José Solé <jmsole-old@Joses-MacBook-Pro.local>
2018-11-15 19:48:27 +0000

    Added AltGR to Left arrow with Rise. Added hold quote as right shift. Disabled sound and a couple of other things.

--- keyboards/planck/config.h
+++ keyboards/planck/config.h
@@ -63,6 +63,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
+
 /* key combination for command */
 #define IS_COMMAND() ( \
     keyboard_report->mods == (MOD_BIT(KC_LSHIFT) | MOD_BIT(KC_RSHIFT)) \
@@ -82,8 +83,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* disable action features */
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
-//#define NO_ACTION_MACRO
-//#define NO_ACTION_FUNCTION
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_MACRO
+#define NO_ACTION_FUNCTION
 
 #endif
